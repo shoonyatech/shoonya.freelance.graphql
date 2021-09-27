1ed8856dcc8afbd84a8b13b3311d6021b626a760
hussamkhatib <hussamkhatib20@gmail.com>
2021-09-27 22:30:39 +0530

    fix(models/js) : remove required key for everythin g other than name and _id

--- models.js
+++ models.js
@@ -8,51 +8,39 @@ const UserSchema = new Schema({
   },
   title: {
     type : String,
-    required : true,
   },
   picture: {
     type : String,
-    required : true,
   },
   bio: {
     type : String,
-    required : true,
   },
   contact: {
     type : Object,
-    required : true
   },
   professionalExperience : {
     type: Array,
-    required: true
   },
   skills : {
     type: Array,
-    required: true
   },
   education : {
     type: Array,
-    required: true
   },
   developerCommunityInvolement : {
     type: Array,
-    required: true
   },
   languages : {
     type: Array,
-    required: true
   },
   hobbies : {
     type: Object,
-    required: true
   },
   sports : {
     type : Object,
-    required: true
   },
   countriesICanWork : {
     type : Array,
-    required: true
   }
 });
 

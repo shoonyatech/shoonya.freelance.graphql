46f4a208ff7553b5031b949eaa571c2fd146d722
hussamkhatib <hussamkhatib20@gmail.com>
2022-01-03 23:52:59 +0530

    feat(projects): add agregate func in resolver for search func , temp remove skill,budget filters in it

--- server.js
+++ server.js
@@ -18,7 +18,13 @@ async function startApolloServer(typeDefs, resolvers) {
       const token = getToken?.slice(7);
       const user = jwt.decode(token);
       const userId = user?.sub.split("|")[1];
-      return { userId };
+      // console.log({ userId })
+      // return { userId }
+
+      // return { userId: "61bd832be09c83006f188641" };  // client
+      return { userId: "61b46fc9f3fb95006a72eb98" };  // client 2
+      // return { userId: "61c0556c36bf6500715d510c" };  //freelancer
+      // return { userId: "61d17cf236bf6500715fa022" };  //freelancer 2
     },
     playground: {
       settings: {
@@ -32,7 +38,8 @@ async function startApolloServer(typeDefs, resolvers) {
 
   mongoose
     .connect(
-      `mongodb+srv://shoonya-test:shoonya-test@cluster0.sa0ig.mongodb.net/shoonya?retryWrites=true&w=majority`,
+      // `mongodb+srv://shoonya-test:shoonya-test@cluster0.sa0ig.mongodb.net/shoonya?retryWrites=true&w=majority`,
+      `mongodb+srv://shoonya-test:shoonya-test@cluster0.zfynb.mongodb.net`,
       { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then((res) => {
